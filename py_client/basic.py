b024fc91c0bc36c38628138996e2961f711e9d1e
demo-23home <zeyadslama23@gmail.com>
2023-10-24 02:48:36 +0300

    data injestion using DRF

--- py_client/basic.py
+++ py_client/basic.py
@@ -4,8 +4,8 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/" #http://127.0.0.1:8000/ 
 
-get_response = requests.get(endpoint, json={"product_id": 123 }) # HTTP Request
-# print(get_response.text) # print raw text response
+get_response = requests.post(endpoint, json={"title":"aBc123","content": "hello world","price":"1c2" }) # HTTP Request
+# print(get_response.text) # print raw text "response"
 # print(get_response.status_code)
 
 # HTTP Request -> HTML
