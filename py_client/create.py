c23833f58edab857804d05d62eab034e5ff12e68
demo-23home <zeyadslama23@gmail.com>
2023-10-24 14:19:08 +0300

    generic_CreateAPIView

---
+++ py_client/create.py
@@ -0,0 +1,17 @@
+import requests
+
+
+
+endpoint = "http://localhost:8000/api/products/"    
+
+
+data = {
+    "title":"This filed is done",
+    "price":120
+}
+
+
+get_response = requests.post(endpoint,json=data)
+
+
+print(get_response.json())
